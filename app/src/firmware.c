fd433b8e68e4142f97d31d3f2808567bc51cda82
kushal-kj <kushal.kuramkote@gmail.com>
2025-07-08 21:17:57 +0530

    feat: Implemented systick and interrupt
    1. Created a function called systick setup where i set the systick frequency
    by passing both CPU and systick frequencies as parameter. Enable the counter
    to track the time and also enabled the interrupt.
    
    2. Created get_ticks() function to fetch the ticks which is a counter variable
    which gets incremented inside the systick_handler function.
    
    3. In main(), i called the systick_setup() and in infinite loop, I will be
    toggling LED for every 1 sec when systick reaches 1000 ticks as systick is
    set to tick for every 1ms.

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,5 +1,26 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
+
+
+#define LED_PORT		(GPIOA)
+#define LED_PIN			(GPIO5)
+
+#define CPU_FREQ		(84000000)
+#define SYSTICK_FREQ	(1000)
+
+volatile uint64_t ticks = 0;
+
+void sys_tick_handler(void)
+{
+	ticks++;
+}
+
+static uint64_t get_ticks(void)
+{
+	return ticks;
+}
 
 static void rcc_setup(void)
 {
@@ -9,18 +30,33 @@ static void rcc_setup(void)
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+}
+
+static void systick_setup(void)
+{
+	systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+	systick_counter_enable();
+	systick_interrupt_enable();
+	
 }
 
 int main(void)
 {
 	rcc_setup();
 	gpio_setup();
+	systick_setup();
+
+	uint64_t start_time = get_ticks();
 
 	while(1)
 	{
-		gpio_toggle(GPIOA, GPIO5);
-		for(int i=0; i< 1000000; i++) __asm__("nop");
+		if(get_ticks() - start_time >= 1000)
+		{
+			gpio_toggle(LED_PORT, LED_PIN);
+			start_time = get_ticks();
+		}
+		
 
 	}
 	return 0;
